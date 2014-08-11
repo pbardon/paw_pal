6bdad134b810527b40d8c9cdc2d9ec60a9dbe7c0
Peter Bardon <pbard42@uw.edu>
2014-08-11 00:50:26 -0700

    removing extraneous map code

--- app/assets/javascripts/views/sitters/sitter_show.js
+++ app/assets/javascripts/views/sitters/sitter_show.js
@@ -11,7 +11,6 @@ DogSittingApp.Views.SitterShow = Backbone.CompositeView.extend({
 
     this.listenTo(this.model.comments(), 'add', this.render);
 
-    this.addMap();
   },
 
   className: "sitterShow",
@@ -106,41 +105,6 @@ DogSittingApp.Views.SitterShow = Backbone.CompositeView.extend({
   redirectToEdit: function(event) {
     var data = $(event.currentTarget).data('id');
     Backbone.history.navigate('#/sitters/' + data + '/edit', {trigger: true});
-  },
-
-  placeMark: function() {
-    var map = this.map;
-
-    var image = {
-      url: 'https://s3-us-west-1.amazonaws.com/pet-sitter-development/paw_icon3.png',
-      size: new google.maps.Size(20, 20),
-      origin: new google.maps.Point(0,0),
-      anchor: new google.maps.Point(0, 20)
-    };
-
-    var shape = {
-      coords: [1,1,1,20,20,20,20,1],
-      type: 'poly'
-    };
-
-    var lat = this.model.get('latitude'),
-        lng = this.model.get('longitude');
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat, lng),
-      icon: image,
-      map: map,
-      shape: shape,
-      title: this.model.get('sitter_name')
-    });
-
-  },
-
-  addMap: function() {
-    var mapview = new DogSittingApp.Views.MapShow({
-      model: this.model
-    });
-
-    this.addSubview('.mapWrapper', mapview);
   },
 
   render: function() {
