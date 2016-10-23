f568ac712c464a7e01b64c0f80844094b96c39bd
Peter Bardon <pbard42@uw.edu>
2016-10-23 00:59:12 -0700

    used ng repeat to populate dog index and enable nav buttons

--- app/assets/javascripts/app/scripts/controllers/dogsController.js
+++ app/assets/javascripts/app/scripts/controllers/dogsController.js
@@ -7,6 +7,27 @@ define('controllers/dogsController', ['controllers/controllers', 'services/dogSe
             });
         };
 
-        $scope.dogs = []
+        $scope.nextPage = function() {
+            if ($scope.dogs.length - 1 > $scope.maxPageIdx) {
+                $scope.minPageIdx += 4;
+                $scope.maxPageIdx += 4;
+            }
+        };
+
+        $scope.previousPage = function () {
+            if ($scope.minPageIdx > 3) {
+                $scope.minPageIdx -= 4;
+                $scope.maxPageIdx -= 4;
+            }
+        };
+
+        $scope.isOnPage = function(index) {
+            return (index >= $scope.minPageIdx && index <= $scope.maxPageIdx);
+        };
+
+        $scope.dogs = [];
+
+        $scope.minPageIdx = 0;
+        $scope.maxPageIdx = 3;
     }]);
 });
